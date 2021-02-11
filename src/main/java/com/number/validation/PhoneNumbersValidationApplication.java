1cff1c7c8ce4f2e44e25bd55cf75f56cf92a8958
Reda Tawfik <reda.rm148@gmail.com>
2021-02-11 02:37:02 +0200

    Initial commit

---
+++ src/main/java/com/number/validation/PhoneNumbersValidationApplication.java
@@ -0,0 +1,13 @@
+package com.number.validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneNumbersValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneNumbersValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/number/validation/PhoneNumbersValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.number.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneNumbersValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
